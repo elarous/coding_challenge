f32620dfa9fdbf3cd6856eabc6b57c81b65aab42
elarouss <elarbaouioussama@gmail.com>
2018-11-30 15:16:12 +0100

    refactor tests and use shared variables, with cleaning hook for each test

--- test/server/db_test.js
+++ test/server/db_test.js
@@ -9,8 +9,32 @@ let store = null;
 let testdb = null;
 const dbUrl = 'mongodb://localhost/ccdb';
 
+/* users */
+const samiPassword = '12345';
+const samiEmail = 'sami@sami.com';
+let sami = null;
+
+const jamesEmail = 'james@james.com';
+const jamesPassword = 'abcde';
+
+/* location */
+const marrakesh = { long: -7.973328, lat: 31.669746 };
+
+/* shops */
+const moscowName = 'Moscow Shop';
+const moscowImage = 'moscow_img.jpg';
+const moscowCoords = { long: 37.618423, lat: 55.751244 };
+
+const parisName = 'Paris Shop';
+const parisImage = 'paris_img.jpg';
+const parisCoords = { lat: 48.864716, long: 2.349014 };
+
+let moscowShop = null;
+let parisShop = null;
+
+
 describe('Testing the persistence layer (db Store)', () => {
-  before(() => {
+  before(async () => {
     store = new Store(dbUrl);
     testdb = store.db;
     testdb.collection('users').remove({});
@@ -21,26 +45,28 @@ describe('Testing the persistence layer (db Store)', () => {
     testdb.close();
   });
 
-  beforeEach(function beforeEachHook() {
+  beforeEach(async function beforeEachHook() {
     this.timeout(500);
-    testdb.collection('users').remove({});
-    testdb.collection('shops').remove({});
+    await testdb.collection('users').remove({});
+    await testdb.collection('shops').remove({});
+
+    sami = await store.saveUser(samiEmail, samiPassword);
+    moscowShop = await store.saveShop(moscowName, moscowImage, moscowCoords); 
   });
 
   describe('Saving a User', () => {
     it('Should insert a user with the right infos', async () => {
       // Given
-      const email = 'test@test.com';
-      const password = '123456';
+      /* james credentials */
 
       // When
-      const user = await store.saveUser(email, password);
+      const james = await store.saveUser(jamesEmail, jamesPassword);
 
       // Then
-      expect(user.email).to.equal(email);
-      expect(user.password).to.equal(password);
-      expect(user.preferredShops).to.be.an('array').that.is.empty;
-      expect(user.dislikedShops).to.be.an('array').that.is.empty;
+      expect(james.email).to.equal(jamesEmail);
+      expect(james.password).to.equal(jamesPassword);
+      expect(james.preferredShops).to.be.an('array').that.is.empty;
+      expect(james.dislikedShops).to.be.an('array').that.is.empty;
     });
   });
 
@@ -58,27 +84,23 @@ describe('Testing the persistence layer (db Store)', () => {
 
     it('Should return a the correct user using email', async () => {
       // Given
-      const email = 'existing@existing.com';
-      const password = 'random_password';
-      await store.saveUser(email, password);
+      /* sami is in DB */
 
       // When
-      const user = await store.loadUser(email);
+      const user = await store.loadUser(samiEmail);
 
       // Then
-      expect(user.email).to.equal(email);
-      expect(user.password).to.equal(password);
+      expect(user.email).to.equal(samiEmail);
+      expect(user.password).to.equal(samiPassword);
     });
 
     it('Should return null when for a matching email and non-matching password', async () => {
       // Given
-      const email = 'another@another.com';
-      const password = 'random_password';
-      const differentPassword = 'different_password';
-      await store.saveUser(email, password);
+      /* sami is in DB */
+      const notSamiPassword = 'different_password';
 
       // When
-      const user = await store.loadUser(email, differentPassword);
+      const user = await store.loadUser(samiEmail, notSamiPassword);
 
       // Then
       expect(user).to.be.null;
@@ -86,94 +108,88 @@ describe('Testing the persistence layer (db Store)', () => {
 
     it('Should return user using email and password', async () => {
       // Given
-      const email = 'exist@exist.com';
-      const password = 'a_password';
-      await store.saveUser(email, password);
+      /* sami is in DB */
 
       // When
-      const user = await store.loadUser(email, password);
+      const user = await store.loadUser(samiEmail, samiPassword);
 
       // Then
-      expect(user.email).to.equal(email);
-      expect(user.password).to.equal(password);
+      expect(user.email).to.equal(samiEmail);
+      expect(user.password).to.equal(samiPassword);
     });
   });
 
   describe('Saving a Shop', () => {
     it('Should insert a shop with the right infos', async () => {
       // Given
-      const name = 'Shop 1';
-      const image = 'img1.jpg';
-      const coords = { lat: 1.5, long: 2.3 };
+      /* paris shop infos */
 
       // When
-      const shop = await store.saveShop(name, image, coords);
+      const shop = await store.saveShop(parisName, parisImage, parisCoords);
 
       // Then
-      expect(shop.name).to.equal(name);
-      expect(shop.image).to.equal(image);
-      expect(shop.location.coordinates).to.include.members([coords.lat, coords.long]);
+      expect(shop.name).to.equal(parisName);
+      expect(shop.image).to.equal(parisImage);
+      expect(shop.location.coordinates).to.include.members([parisCoords.lat, parisCoords.long]);
     });
   });
 
   describe('Retrieving a Shop', () => {
     it("Should return a shop using it's id", async () => {
       // Given
-      const name = 'Shop 2';
-      const image = 'img1.jpg';
-      const coords = { lat: 1.5, long: 2.3 };
-      const shop = await store.saveShop(name, image, coords);
+      /* moscow shop already in db */
 
       // When
-      const retrievedShop = await store.getShopById(shop._id);
+      const retrievedShop = await store.getShopById(moscowShop._id);
 
       // Then
-      expect(retrievedShop.name).to.equal(name);
-      expect(retrievedShop.image).to.equal(image);
-      expect(retrievedShop.location.coordinates).to.include.members([coords.lat, coords.long]);
-      expect(retrievedShop._id).to.deep.equal(shop._id);
+      expect(retrievedShop.name).to.equal(moscowName);
+      expect(retrievedShop.image).to.equal(moscowImage);
+      expect(retrievedShop.location.coordinates)
+        .to.include.members([moscowCoords.lat, moscowCoords.long]);
+      expect(retrievedShop._id).to.deep.equal(moscowShop._id);
     });
   });
 
   describe('Preferred Shops', () => {
     it("Should add a shop to a user's preferred shops", async () => {
       // Given
-      const user = await store.saveUser('test1@test1.com', 'random_pass');
-      const shop = await store.saveShop('Shop 3', 'img1.jpg', { lat: 1.1, long: 2.2 });
+      /* user sami already in db */
+      /* shop moscow alrady in db */
 
       // When
-      const updatedUser = await store.addToPreferred(user._id, shop._id);
+      const updatedUser = await store.addToPreferred(sami._id, moscowShop._id);
 
       // Then
       expect(updatedUser.preferredShops)
         .is.an('array')
-        .that.includes(shop._id);
+        .that.includes(moscowShop._id);
     });
 
     it('Should load preferred shops of the given user', async () => {
       // Given
-      const user = await store.saveUser('test2@test2.com', 'random_pass');
-      const shop1 = await store.saveShop('Shop 4', 'img1.jpg', { lat: 1.1, long: 2.2 });
-      const shop2 = await store.saveShop('Shop 5', 'img2.jpg', { lat: 3.3, long: 4.4 });
-      await store.addToPreferred(user._id, shop1._id);
-      await store.addToPreferred(user._id, shop2._id);
+      /* user sami already in db */
+      /* moscow shop already in db */
+      parisShop = await store.saveShop(parisName, parisImage, parisCoords);
+      await store.addToPreferred(sami._id, moscowShop._id);
+      await store.addToPreferred(sami._id, parisShop._id);
 
       // When
-      const shops = await store.loadPreferredShops(user._id);
+      const shops = await store.loadPreferredShops(sami._id);
 
       // Then
-      expect(shops[0].name).to.equal('Shop 4');
-      expect(shops[1].name).to.equal('Shop 5');
+      expect(shops[0].name).to.equal(moscowName);
+      expect(shops[1].name).to.equal(parisName);
     });
 
     it('Should remove a shop from user\'s preferred list', async () => {
       // Given
-      const user = await store.saveUser('tes3t@test3.com', 'random_pass');
-      const shop = await store.saveShop('Shop 6', 'img1.jpg', { lat: 1.1, long: 2.2 });
-      await store.addToPreferred(user._id, shop._id);
+      /* user sami already in db */
+      /* moscow shop alrady in db */
+      await store.addToPreferred(sami._id, moscowShop._id);
 
       // When
-      const updatedUser = await store.removeFromPreferred(user._id, shop._id);
+      const updatedUser = await store.removeFromPreferred(sami._id, moscowShop._id);
 
       // Then
       expect(updatedUser.preferredShops).is.an('array').that.is.empty;
@@ -183,25 +199,25 @@ describe('Testing the persistence layer (db Store)', () => {
   describe('Disliked Shops', () => {
     it('Should add a shop to user\'s disliked shops list', async () => {
       // Given
-      const user = await store.saveUser('test4@test4.com', 'random_pass');
-      const shop = await store.saveShop('Shop 7', 'img1.jpg', { lat: 1.1, long: 2.2 });
+      /* user sami already in db */
+      /* shop moscow already in db */
 
       // When
-      const updatedUser = await store.addToDisliked(user._id, shop._id);
+      const updatedUser = await store.addToDisliked(sami._id, moscowShop._id);
 
       // Then
       expect(updatedUser.dislikedShops).is.an('array').that.have.lengthOf(1);
-      expect(updatedUser.dislikedShops[0].shop).is.equal(shop._id.toString());
+      expect(updatedUser.dislikedShops[0].shop).is.equal(moscowShop._id.toString());
     });
 
     it('Should add a timestamp when adding a shop to the user\'s disliked list', async () => {
       // Given
-      const user = await store.saveUser('test5@test5.com', 'random_password');
-      const shop = await store.saveShop('Shop 8', 'img1.jpg', { lat: 1.1, long: 2.2 });
+      /* user sami already in db */
+      /* shop moscow already in db */
       const currentTime = new Date();
 
       // When
-      const updatedUser = await store.addToDisliked(user._id, shop._id);
+      const updatedUser = await store.addToDisliked(sami._id, moscowShop._id);
 
       // Then
       expect(updatedUser.dislikedShops[0].timestamp).is.a('Date');
@@ -212,9 +228,8 @@ describe('Testing the persistence layer (db Store)', () => {
   describe('Listing Shops', () => {
     it('Should get nearest shops', async () => {
       // Given
-      const moscowShop = await store.saveShop('Shop 9', 'img1.jpg', { long: 37.618423, lat: 55.751244 });
-      const parisShop = await store.saveShop('Shop 10', 'img1.jpg', { lat: 48.864716, long: 2.349014 });
-      const marrakesh = { long: -7.973328, lat: 31.669746 };
+      /* moscow shop alrady in db */
+      parisShop = await store.saveShop(parisName, parisImage, parisCoords);
 
       // When
       const shops = await store.nearShops(marrakesh);
@@ -226,14 +241,13 @@ describe('Testing the persistence layer (db Store)', () => {
 
     it('Should filter out disliked shops of the nearest shops', async () => {
       // Given
-      const shop = await store.saveShop('Shop 10', 'img1.jpg', { lat: 48.864716, long: 2.349014 });
-      const user = await store.saveUser('test6@test6.com', 'random_pass');
-      await store.addToDisliked(user._id, shop._id);
-      const marrakesh = { long: -7.973328, lat: 31.669746 };
+      /* user sami already in db */
+      /* moscow shop already in db */
+      await store.addToDisliked(sami._id, moscowShop._id);
       const nearShops = await store.nearShops(marrakesh);
 
       // When
-      const newShops = await store.filterOutDisliked(user._id, nearShops);
+      const newShops = await store.filterOutDisliked(sami._id, nearShops);
 
       // Then
       expect(newShops).to.be.an('array').that.have.lengthOf(0);
