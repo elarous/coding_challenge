cb8d9f89cc2b70166d8e7160a4baf2a5585cf5c5
elarouss <elarbaouioussama@gmail.com>
2018-12-01 08:31:09 +0100

    add test for login with wrong credentials

--- test/server/test.js
+++ test/server/test.js
@@ -1,2 +1,23 @@
 /* eslint-disable no-undef */
-const { expect } = require('chai');
+import chai from 'chai';
+import { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../src/server/index';
+
+chai.use(chaiHttp);
+
+describe('User Authentication', () => {
+  describe('User can login using email and password', () => {
+    it('Should return unauthorized error when trying login with wrong credentils', (done) => {
+      chai
+        .request(server)
+        .post('/login')
+        .send({ username: 'random@random.com', password: '12345' })
+        .end((err, res) => {
+          expect(res).to.have.status(401);
+          expect(res.text).to.equal('Unauthorized');
+          done();
+        });
+    });
+  });
+});
