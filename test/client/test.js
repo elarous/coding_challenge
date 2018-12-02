f70a87a6d50f9856984f6d4fa980b03416548de3
elarouss <elarbaouioussama@gmail.com>
2018-12-02 08:51:01 +0100

    prepare for client side development

---
+++ test/client/test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
+import '@babel/polyfill';
+import chai, { expect } from 'chai';
+
+describe('test client', () => {
+  it('should work ', () => {
+    expect(true).to.be.true;
+  });
+});
