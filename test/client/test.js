fb8e8e03004b2009082ae8791a654196cd7c3346
elarouss <elarbaouioussama@gmail.com>
2018-12-03 10:56:54 +0100

    add basic tests for <LoginForm />

--- test/client/test.js
+++ test/client/test.js
@@ -1,3 +1,22 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-expressions */
 import '@babel/polyfill';
+import React from 'react';
+import { expect } from 'chai';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { LoginForm } from '../../src/client/login';
+import { RegisterForm } from '../../src/client/register';
+
+import { FormInput, FormButton } from '../../src/client/forms';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Testing <LoginForm />', () => {
+  it('Should renderer 2 <FormInput /> components', () => {
+    const wrapper = shallow(<LoginForm />);
+    expect(wrapper.find(FormInput)).to.have.lengthOf(2);
+    expect(wrapper.find(FormButton)).to.have.lengthOf(1);
+  });
+});
